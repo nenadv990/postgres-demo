9cc75ca9d776f14a13f5f24d4ba4276e324260ca
Nenad <nenadv990@gmail.com>
2019-12-08 19:54:25 +0100

    Update PostgresDemoApplication.java

--- src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
+++ src/main/java/com/example/postgresdemo/PostgresDemoApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 public class PostgresDemoApplication {
     public static void main(String[] args) {
+    	System.out.println("HELLO!");
         SpringApplication.run(PostgresDemoApplication.class, args);
     }
 }
